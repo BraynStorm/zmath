20fcebf4aa987944829f5ec037d645bea4f58d0f
Bozhidar Stoyanov <bojidar.b.stoyanov@gmail.com>
2025-03-09 15:44:18 +0200

    fix: Compatibility with Zig 0.14

--- src/zmath.zig
+++ src/zmath.zig
@@ -329,7 +329,7 @@ pub inline fn boolx16(
 // zig fmt: on
 
 pub inline fn veclen(comptime T: type) comptime_int {
-    return @typeInfo(T).Vector.len;
+    return @typeInfo(T).vector.len;
 }
 
 pub inline fn splat(comptime T: type, value: f32) T {
@@ -413,14 +413,14 @@ pub inline fn storeArr4(arr: *[4]f32, v: F32x4) void {
 }
 
 pub inline fn arr3Ptr(ptr: anytype) *const [3]f32 {
-    comptime assert(@typeInfo(@TypeOf(ptr)) == .Pointer);
+    comptime assert(@typeInfo(@TypeOf(ptr)) == .pointer);
     const T = std.meta.Child(@TypeOf(ptr));
     comptime assert(T == F32x4);
     return @as(*const [3]f32, @ptrCast(ptr));
 }
 
 pub inline fn arrNPtr(ptr: anytype) [*]const f32 {
-    comptime assert(@typeInfo(@TypeOf(ptr)) == .Pointer);
+    comptime assert(@typeInfo(@TypeOf(ptr)) == .pointer);
     const T = std.meta.Child(@TypeOf(ptr));
     comptime assert(T == Mat or T == F32x4 or T == F32x8 or T == F32x16);
     return @as([*]const f32, @ptrCast(ptr));
@@ -4246,9 +4246,9 @@ fn fftUnswizzle(input: []const F32x4, output: []F32x4) void {
             const n = index / 2;
             var addr =
                 (((@as(usize, @intCast(static.swizzle_table[n & 0xff])) << 24) |
-                (@as(usize, @intCast(static.swizzle_table[(n >> 8) & 0xff])) << 16) |
-                (@as(usize, @intCast(static.swizzle_table[(n >> 16) & 0xff])) << 8) |
-                (@as(usize, @intCast(static.swizzle_table[(n >> 24) & 0xff])))) >> rev32) |
+                    (@as(usize, @intCast(static.swizzle_table[(n >> 8) & 0xff])) << 16) |
+                    (@as(usize, @intCast(static.swizzle_table[(n >> 16) & 0xff])) << 8) |
+                    (@as(usize, @intCast(static.swizzle_table[(n >> 24) & 0xff])))) >> rev32) |
                 ((index & 1) * rev7 * 4);
             f32_output[addr] = input[index][0];
             addr += rev7;
